261e7ac5b21bd1dee2159bf33b4031e66fb186c2
busyhuman <busyhuman@naver.com>
2020-11-21 18:13:27 +0900

    Update serializers.py

--- SIS/SpaceServer/serializers.py
+++ SIS/SpaceServer/serializers.py
@@ -1,14 +1,20 @@
 from rest_framework import serializers
-from .models import User, Record
+from .models import *
 from django import forms
 
 class UserSerializer(serializers.ModelSerializer):
     PW = forms.CharField(widget=forms.PasswordInput)
-    class Meta : 
+    class Meta :
         model = User
         fields = '__all__'
 
 class RecordSerializer(serializers.ModelSerializer):
-    class Meta : 
+    class Meta :
         model = Record
+        fields = '__all__'
+
+
+class LoginedSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Logined
         fields = '__all__'
