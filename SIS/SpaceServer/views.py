54e693a6df5db88de03508d0ad7ccc23fa100e48
busyhuman <busyhuman@naver.com>
2020-11-21 18:14:24 +0900

    Update views.py

--- SIS/SpaceServer/views.py
+++ SIS/SpaceServer/views.py
@@ -1,17 +1,32 @@
 from django_filters import rest_framework, NumberFilter
 import django
-from .models import User, Record
+from .models import *
 from rest_framework import viewsets, generics
-from .serializers import UserSerializer, RecordSerializer
+from .serializers import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     filter_backends = (rest_framework.DjangoFilterBackend, )
-    filter_fields = ('ID', 'PW',)
+    filter_fields = ('ID',)
 
 class RecordViewSet(viewsets.ModelViewSet):
-    queryset = Record.objects.all()
+    queryset = Record.objects.all().order_by('-Score', 'RecordNum')[:10]
     serializer_class = RecordSerializer
     filter_backends = (rest_framework.DjangoFilterBackend, )
-    filter_fields = ('RecordNum', 'user', 'Date', 'Score',)
+    filter_fields = ('RecordNum', 'user', 'Date', 'Score', 'Stage', )
+
+
+class LoginedViewSet(viewsets.ModelViewSet):
+    queryset = Logined.objects.all()
+    serializer_class = LoginedSerializer
+    filter_backends = (rest_framework.DjangoFilterBackend, )
+    filter_fields = ('user', )
+
+class wow(APIView):
+    def get(self, request):
+        print(request.user)
+        print(request.auth)
+        return Response(request.user)
