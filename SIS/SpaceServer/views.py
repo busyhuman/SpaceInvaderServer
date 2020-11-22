81d58ef8402eb28bad164778040883426443b0d7
busyhuman <busyhuman@naver.com>
2020-11-23 03:13:14 +0900

    Update views.py

--- SIS/SpaceServer/views.py
+++ SIS/SpaceServer/views.py
@@ -7,27 +7,30 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import HttpResponse
 
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from django.shortcuts import get_object_or_404
+
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     filter_backends = (rest_framework.DjangoFilterBackend, )
     filter_fields = ('ID',)
 
-class RecordViewSet(viewsets.ModelViewSet):
-    queryset = Record.objects.all().order_by('-Score', 'RecordNum')[:10]
-    serializer_class = RecordSerializer
-    filter_backends = (rest_framework.DjangoFilterBackend, )
-    filter_fields = ('RecordNum', 'user', 'Date', 'Score', 'Stage', )
 
-
-class LoginedViewSet(viewsets.ModelViewSet):
-    queryset = Logined.objects.all()
-    serializer_class = LoginedSerializer
-    filter_backends = (rest_framework.DjangoFilterBackend, )
-    filter_fields = ('user', )
-
-class wow(APIView):
+class RecordViewSet(APIView):
     def get(self, request):
+        queryset = Record.objects.all().order_by('-Score', 'RecordNum')[:10]
+        serializer = RecordSerializer(queryset, many=True)
+        return Response(serializer.data);
+      #  serializer_class = RecordSerializer
+      #  filter_backends = (rest_framework.DjangoFilterBackend, )
+      #  filter_fields = ('RecordNum', 'user', 'Date', 'Score', 'Stage', )
+
+    def post(self, request):
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         print(request.user)
-        print(request.auth)
-        return HttpResponse(request.user)
+        queryset = Record.objects.create(user=get_object_or_404(User, pk=request.user), Stage=request.data['Stage'], Score=request.data['Score'])
+
+        return Response("Being Added !!")
