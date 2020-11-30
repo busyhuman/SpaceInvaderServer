20924039693ba98e4a3587998c0fd926d47cb7e5
busyhuman <busyhuman@naver.com>
2020-11-30 21:18:25 +0900

    Update views.py

--- SIS/SpaceServer/views.py
+++ SIS/SpaceServer/views.py
@@ -19,16 +19,64 @@ class UserViewSet(viewsets.ModelViewSet):
     filter_fields = ('ID',)
 
 
-class RecordViewSet(APIView):
+class RecordListView(APIView):
     def get(self, request):
-        queryset = Record.objects.all().order_by('-Score', 'RecordNum')[:10]
+        ListCount = 10
+        queryset = Record.objects.all()
+
+        mRecordNum = request.GET.get('RecordNum')
+        mScore = request.GET.get('Score')
+        mScore = request.GET.get('Stage')
+        muser = request.GET.get('user')
+        mDate = request.GET.get('Date')
+
+        if request.GET.get('RecordNum') is not None:
+            queryset = queryset.filter(RecordNum = request.GET.get('RecordNum'))
+
+        if request.GET.get('Score') is not None:
+            queryset = queryset.filter(Score = request.GET.get('Score'))
+
+        if request.GET.get('Stage') is not None:
+            queryset = queryset.filter(Stage = request.GET.get('Stage'))
+
+        if request.GET.get('user') is not None:
+            queryset = queryset.filter(user = request.GET.get('user'))
+
+        if request.GET.get('Date') is not None:
+            queryset = queryset.filter(Date = request.GET.get('Date'))
+
+        queryset = queryset.order_by('-Score', 'RecordNum')[:ListCount]
+
         serializer = RecordSerializer(queryset, many=True)
         return Response(serializer.data);
-      #  serializer_class = RecordSerializer
-      #  filter_backends = (rest_framework.DjangoFilterBackend, )
-      #  filter_fields = ('RecordNum', 'user', 'Date', 'Score', 'Stage', )
+
 
     def post(self, request):
+        print(request.user)
         queryset = Record.objects.create(user=get_object_or_404(User, pk=request.user), Stage=request.data['Stage'], Score=request.data['Score'])
 
         return Response("Being Added !!")
+
+
+class RecordDetailView(APIView):
+    def get(self, request, rid):
+        queryset = Record.objects.filter(RecordNum = rid).order_by('-Score', 'RecordNum')
+        serializer = RecordSerializer(queryset, many=True)
+        return Response(serializer.data);
+
+
+class Auth(APIView):
+    def get(self, request):
+        print(request.user)
+        print(request.auth)
+        return HttpResponse(request.user)
+
+    def post(self, request):
+        print("data =", request.data['id'])
+        user = authenticate(username=request.data['id'], password=request.data['password'])
+        print("user =", user)
+        if user is not None:
+            token = Token.objects.get(user=user)
+            return HttpResponse(token.key)
+        else:
+            return HttpResponse("shit~")
